203ab2640510037ac81f12664fa6d5789dc9e99d
Lara Scremin <lscreminmendes@gmail.com>
2025-04-22 21:34:56 -0300

    login desktop layout

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,53 +1,42 @@
 import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 import PopUp from "../../components/PopUp";
-import logo from "../../assets/images/colored-logo.svg";
 import StandardInput from "../../components/Standard Input";
 import PasswordInput from "../../components/Password Input";
 import HALF_MOON_SGV from "../../assets/vectors/half-moon";
 import TRIANGLE_SVG from "../../assets/vectors/triangle";
-import { Circle } from "phosphor-react";
 import CIRCLE_SVG from "../../assets/vectors/plus";
+import LoginHeader from "../../components/Login Header";
+import PLUS_SVG from "../../assets/vectors/plus";
+import HEART_SVG from "../../assets/vectors/heart";
 
 function Login() {
   const isMobile = useMediaQuery({ maxWidth: 768 });
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleEntrar = () => {
+    navigate("/home");
+  };
 
   return (
     <>
       {isMobile ? (
         <div className="flex-col login-mbl">
-          <div className="login-vectors">
-            <HALF_MOON_SGV
-              width={92}
-              height={48}
-              color="var(--green)"
-              className="login-v01"
-            />
+          <div className="sob-overflow login-vectors">
+            <HALF_MOON_SGV color="var(--green)" className="login-v01" />
             <div className="vectors-wrap">
-              <TRIANGLE_SVG
-                width={76}
-                height={64}
-                color="var(--l-blue)"
-                className="login-v02"
-              />
-              <CIRCLE_SVG
-                width={75}
-                height={72}
-                color="var(--l-yellow)"
-                className="login-v03"
-              />
+              <TRIANGLE_SVG color="var(--l-blue)" className="login-v02" />
+              <PLUS_SVG color="var(--l-yellow)" className="login-v03" />
             </div>
           </div>
           <PopUp>
             <form className="flex-col login-form ">
-              <div className="flex-col centralized">
-                <img src={logo} className="resized-logo" />
-                <h4>Bem vindo(a) de volta!</h4>
-              </div>
+              <LoginHeader title="Bem vindo(a) de volta!" />
               <div className="login-input-wrap">
                 <StandardInput
                   label={"Insira seu e-mail"}
@@ -63,18 +52,71 @@ function Login() {
                 <a className="reset-link">Esqueceu sua senha?</a>
               </div>
               <div className="flex-col login-button-wrap">
-                <button type="submit" className="std-button">
+                <button
+                  type="submit"
+                  className="std-button"
+                  onClick={handleEntrar}
+                >
                   ENTRAR
                 </button>
                 <h6>
-                  Não possui uma conta? <a>Cadastre-se Aqui</a>
+                  Não possui uma conta? <a href="/register">Cadastre-se Aqui</a>
                 </h6>
               </div>
             </form>
           </PopUp>
         </div>
       ) : (
-        <p>desktop</p>
+        <div className="login-dsk">
+          <div className="flex-col login-left">
+            <p className="decoration-text">
+              “A transformação social
+              <br /> começa com um gesto:
+              <br /> o de <span>doar</span>.”
+            </p>
+            <div className="sob-overflow login-vectors">
+              <div className="sob-overflow vectors-wrap">
+                <TRIANGLE_SVG color="var(--l-blue)" className="login-v02" />
+                <PLUS_SVG color="var(--l-yellow)" className="login-v03" />
+              </div>
+              <div className="sob-overflow vectors-wrap">
+                <HEART_SVG color="var(--l-pink)" className="login-v04" />
+                <HALF_MOON_SGV color="var(--green)" className="login-v01" />
+              </div>
+            </div>
+          </div>
+          <div className="flex-col login-right">
+            <form className="flex-col login-form">
+              <LoginHeader title="Bem vindo(a) de volta!" />
+              <div className="login-input-wrap">
+                <StandardInput
+                  label={"Insira seu e-mail"}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="E-mail"
+                />
+                <PasswordInput
+                  label={"Insira sua senha"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <a className="reset-link">Esqueceu sua senha?</a>
+              </div>
+              <div className="flex-col login-button-wrap">
+                <button
+                  type="submit"
+                  className="std-button"
+                  onClick={handleEntrar}
+                >
+                  ENTRAR
+                </button>
+                <h6>
+                  Não possui uma conta? <a href="/register">Cadastre-se Aqui</a>
+                </h6>
+              </div>
+            </form>
+          </div>
+        </div>
       )}
     </>
   );

52f0d6f4648b8f8890d386d3ffa9bb1b42393561
Lara Scremin <lscreminmendes@gmail.com>
2025-04-30 19:08:39 -0300

    auth pages layouts

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,15 +2,11 @@ import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
-import PopUp from "../../components/PopUp";
 import StandardInput from "../../components/Standard Input";
 import PasswordInput from "../../components/Password Input";
-import HALF_MOON_SGV from "../../assets/vectors/half-moon";
-import TRIANGLE_SVG from "../../assets/vectors/triangle";
-import CIRCLE_SVG from "../../assets/vectors/plus";
 import LoginHeader from "../../components/Login Header";
-import PLUS_SVG from "../../assets/vectors/plus";
-import HEART_SVG from "../../assets/vectors/heart";
+import DesktopLogin from "../../components/Desktop Login";
+import MobileLogin from "../../components/Mobile Login";
 
 function Login() {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -26,97 +22,75 @@ function Login() {
   return (
     <>
       {isMobile ? (
-        <div className="flex-col login-mbl">
-          <div className="sob-overflow login-vectors">
-            <HALF_MOON_SGV color="var(--green)" className="login-v01" />
-            <div className="vectors-wrap">
-              <TRIANGLE_SVG color="var(--l-blue)" className="login-v02" />
-              <PLUS_SVG color="var(--l-yellow)" className="login-v03" />
+        <MobileLogin>
+          <form className="flex-col login-form ">
+            <LoginHeader title="Bem vindo(a) de volta!" />
+            <div className="login-input-wrap">
+              <StandardInput
+                label={"Insira seu e-mail"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="E-mail"
+              />
+              <PasswordInput
+                label={"Insira sua senha"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <a className="reset-link" href="/auth/reset-password">
+                Esqueceu sua senha?
+              </a>
             </div>
-          </div>
-          <PopUp>
-            <form className="flex-col login-form ">
-              <LoginHeader title="Bem vindo(a) de volta!" />
-              <div className="login-input-wrap">
-                <StandardInput
-                  label={"Insira seu e-mail"}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="E-mail"
-                />
-                <PasswordInput
-                  label={"Insira sua senha"}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <a className="reset-link">Esqueceu sua senha?</a>
-              </div>
-              <div className="flex-col login-button-wrap">
-                <button
-                  type="submit"
-                  className="std-button"
-                  onClick={handleEntrar}
-                >
-                  ENTRAR
-                </button>
-                <h6>
-                  Não possui uma conta? <a href="/register">Cadastre-se Aqui</a>
-                </h6>
-              </div>
-            </form>
-          </PopUp>
-        </div>
+            <div className="flex-col login-button-wrap">
+              <button
+                type="submit"
+                className="std-button"
+                onClick={handleEntrar}
+              >
+                ENTRAR
+              </button>
+              <h6>
+                Não possui uma conta?{" "}
+                <a href="/auth/register">Cadastre-se Aqui</a>
+              </h6>
+            </div>
+          </form>
+        </MobileLogin>
       ) : (
-        <div className="login-dsk">
-          <div className="flex-col login-left">
-            <p className="decoration-text">
-              “A transformação social
-              <br /> começa com um gesto:
-              <br /> o de <span>doar</span>.”
-            </p>
-            <div className="sob-overflow login-vectors">
-              <div className="sob-overflow vectors-wrap">
-                <TRIANGLE_SVG color="var(--l-blue)" className="login-v02" />
-                <PLUS_SVG color="var(--l-yellow)" className="login-v03" />
-              </div>
-              <div className="sob-overflow vectors-wrap">
-                <HEART_SVG color="var(--l-pink)" className="login-v04" />
-                <HALF_MOON_SGV color="var(--green)" className="login-v01" />
-              </div>
+        <DesktopLogin>
+          <form className="flex-col login-form">
+            <LoginHeader title="Bem vindo(a) de volta!" />
+            <div className="login-input-wrap">
+              <StandardInput
+                label={"Insira seu e-mail"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="E-mail"
+              />
+              <PasswordInput
+                label={"Insira sua senha"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <a className="reset-link" href="/auth/reset-password">
+                Esqueceu sua senha?
+              </a>
             </div>
-          </div>
-          <div className="flex-col login-right">
-            <form className="flex-col login-form">
-              <LoginHeader title="Bem vindo(a) de volta!" />
-              <div className="login-input-wrap">
-                <StandardInput
-                  label={"Insira seu e-mail"}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="E-mail"
-                />
-                <PasswordInput
-                  label={"Insira sua senha"}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <a className="reset-link">Esqueceu sua senha?</a>
-              </div>
-              <div className="flex-col login-button-wrap">
-                <button
-                  type="submit"
-                  className="std-button"
-                  onClick={handleEntrar}
-                >
-                  ENTRAR
-                </button>
-                <h6>
-                  Não possui uma conta? <a href="/register">Cadastre-se Aqui</a>
-                </h6>
-              </div>
-            </form>
-          </div>
-        </div>
+            <div className="flex-col login-button-wrap">
+              <button
+                type="submit"
+                className="std-button"
+                onClick={handleEntrar}
+              >
+                ENTRAR
+              </button>
+              <h6>
+                Não possui uma conta?{" "}
+                <a href="/auth/register">Cadastre-se Aqui</a>
+              </h6>
+            </div>
+          </form>
+        </DesktopLogin>
       )}
     </>
   );

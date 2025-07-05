f763fcdcb1821adf4a0e7a3ac4853030c6d807b7
Lara Scremin <lscreminmendes@gmail.com>
2025-07-05 14:15:28 -0300

    error dynamic page

--- src/pages/Errors/index.jsx
+++ src/pages/Errors/index.jsx
@@ -7,11 +7,12 @@ import TRIANGLE_SVG from "../../assets/vectors/triangle";
 import HALF_MOON_SGV from "../../assets/vectors/half-moon";
 import HEART_SVG from "../../assets/vectors/heart";
 import POLYGON_SVG from "../../assets/vectors/polygon";
+import STAR_SVG from "../../assets/vectors/start";
 
 function Errors({ errorMessage }) {
   return (
     <div className="error-page">
-      {/* <MOON_SVG color={"var(--l-yellow)"} className="error-v01" /> */}
+      <MOON_SVG color={"var(--l-yellow)"} className="error-v01" />
       <div className="error-content">
         <h1>Ooops!</h1>
         <p>
@@ -23,10 +24,12 @@ function Errors({ errorMessage }) {
         </button>
       </div>
       <div className="error-vectors">
-        {/* <PLUS_SVG color={"var(--l-blue)"} className="error-v02" />
+        <PLUS_SVG color={"var(--l-blue)"} className="error-v02" />
         <HALF_MOON_SGV color={"var(--green)"} className="error-v04" />
+        <POLYGON_SVG color={"var(--yellow)"} className="error-v06" />
         <HEART_SVG color={"var(--l-pink)"} className="error-v05" />
-        <TRIANGLE_SVG color={"var(--l-green)"} className="error-v03" /> */}
+        <TRIANGLE_SVG color={"var(--l-green)"} className="error-v03" />
+        <STAR_SVG color={"var(--l-blue)"} className="error-v07" />
       </div>
     </div>
   );

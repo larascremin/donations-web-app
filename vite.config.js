53f111340e1981d51bd1c61347146bfaeab98e64
Lara Scremin <lscreminmendes@gmail.com>
2025-08-17 20:38:06 -0300

    tailwind plugin config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
 });

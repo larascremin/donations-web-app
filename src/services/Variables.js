86e25c021130f91ee009b4b8b16d2c8f365482a5
Lara Scremin <lscreminmendes@gmail.com>
2025-10-15 21:51:31 -0300

    update lists and style

--- src/services/Variables.js
+++ src/services/Variables.js
@@ -1,1 +1,23 @@
+import {
+  MapPinLine,
+  Shower,
+  CoatHanger,
+  BabyCarriage,
+  Basket,
+} from "@phosphor-icons/react";
+
 export const server = "http://localhost:8080";
+
+export const categoryColors = {
+  higiene: "bg-[var(--l-blue)]",
+  vestuario: "bg-[var(--l-pink)]",
+  alimento: "bg-[var(--l-yellow)]",
+  mobilia: "bg-[var(--l-green)]",
+};
+
+export const categoryIcons = {
+  higiene: Shower,
+  vestuario: CoatHanger,
+  alimento: Basket,
+  mobilia: BabyCarriage,
+};

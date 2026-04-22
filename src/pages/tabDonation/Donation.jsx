829596707e795bc918fdbed7b364b4b7a577728e
Lara Scremin <lscreminmendes@gmail.com>
2026-04-22 19:16:38 -0300

    fix invalid date crash in Donation page

--- src/pages/tabDonation/Donation.jsx
+++ src/pages/tabDonation/Donation.jsx
@@ -44,6 +44,7 @@ function Donation() {
   const formatDate = (dateString) => {
     if (!dateString) return "---";
     const date = new Date(dateString);
+    if (isNaN(date.getTime())) return "---";
     return new Intl.DateTimeFormat("pt-BR", {
       day: "2-digit",
       month: "2-digit",
